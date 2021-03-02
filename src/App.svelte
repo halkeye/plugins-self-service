53f38f7bc00537d3dd22a286b5bd0f3b3d322696
Gavin Mogan <git@gavinmogan.com>
2021-03-02 01:13:49 -0800

    Add code link

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,33 @@
 <script>
 import '@material/mwc-circular-progress';
+import '@material/mwc-top-app-bar';
+import '@material/mwc-icon-button';
+
 import RepoList from './RepoList.svelte';
 import { hasAny } from 'svelte-fetch';
+
 </script>
 
-<main>
-  <RepoList></RepoList>
-  {#if $hasAny}
-  <mwc-dialog heading="Request In Progress" open={true}>
-    <mwc-circular-progress indeterminate={true}></mwc-circular-progress>
-    <p>Working</p>
-  </mwc-dialog>
-  {/if}
-</main>
-
 <style>
+  main {
+    padding: 8px;
+  }
+  .codeLink {
+    color: white;
+  }
 </style>
+
+<mwc-top-app-bar>
+  <div slot="title" id="title">Plugins Self Service</div>
+  <a class="codeLink" slot="actionItems" href="https://github.com/halkeye/plugins-self-service"><mwc-icon-button icon="code" label="github"></mwc-icon-button></a>
+
+  <main>
+    <RepoList></RepoList>
+    {#if $hasAny}
+    <mwc-dialog heading="Request In Progress" open={true}>
+      <mwc-circular-progress indeterminate={true}></mwc-circular-progress>
+      <p>Working</p>
+    </mwc-dialog>
+    {/if}
+  </main>
+</mwc-top-app-bar>
